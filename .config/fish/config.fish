45b734202a15ffb9f29133f1a451dc2bf1ee3469
Aaron Chazz Gonzales <aaroncgonzales.dev@gmail.com>
2021-10-22 19:45:37 +0800

    Added Takuya's peco scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if type -q exa
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+end
+
 starship init fish | source
--- .config/install.sh
+++ .config/install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/sh
 
-ln -sf ~/.dotfiles/.config/fish ~/.config/fish
+ln -sf ~/.dotfiles/.config/fish/* ~/.config/fish
 
-ln -sf ~/.dotfiles/.config/kitty ~/.config/kitty
+ln -sf ~/.dotfiles/.config/fish/functions/* ~/.config/fish/functions
 
-ln -sf ~/.dotfiles/.config/ulauncher ~/.config/ulauncher
+ln -sf ~/.dotfiles/.config/kitty/* ~/.config/kitty
+
+ln -sf ~/.dotfiles/.config/ulauncher/* ~/.config/ulauncher
 
 ln -sf ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
