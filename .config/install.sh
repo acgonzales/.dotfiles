a37992ef9ab75308c11a11d6b867f1f04baa3201
Aaron Chazz Gonzales <aaroncgonzales.dev@gmail.com>
2021-10-22 16:09:07 +0800

    Added ulauncher symlink to install.sh

--- .config/install.sh
+++ .config/install.sh
@@ -4,4 +4,6 @@ ln -sf ~/.dotfiles/.config/fish ~/.config/fish
 
 ln -sf ~/.dotfiles/.config/kitty ~/.config/kitty
 
+ln -sf ~/.dotfiles/.config/ulauncher ~/.config/ulauncher
+
 ln -sf ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
