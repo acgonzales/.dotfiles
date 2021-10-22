568bd2c89499243bc382a01f385a188e4bf31b00
Aaron Chazz Gonzales <aaroncgonzales.dev@gmail.com>
2021-10-22 15:41:53 +0800

    Initial commit

---
+++ .config/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+ln -sf ~/.dotfiles/.config/fish ~/.config/fish
+
+ln -sf ~/.dotfiles/.config/kitty ~/.config/kitty
+
+ln -sf ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
